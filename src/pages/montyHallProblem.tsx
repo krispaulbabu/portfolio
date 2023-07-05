96aa67afc4f27e5ca31c5927a074c54d8e10b2cf
krispaulbabu <krispaulbabukkp@gmail.com>
2023-07-05 14:12:47 +0400

    Made the monty hall page look a little nicer. Added a black background, added some texts, an image of three doors. the link's color etc.,

--- src/pages/montyHallProblem.tsx
+++ src/pages/montyHallProblem.tsx
@@ -16,12 +16,26 @@ export default function montyHallProblem() {
           different doors. Behind 1 of the 3 doors is a car ( or a gift / prize
           money of sorts), and behind the other two doors are goats. The
           contestant is asked to choose one of the doors. On picking a door, say
-          door no.1 , the host, who knows what lies behind each door, reveals
-          another door, say door no.3, which has a goat. The contestant is then
+          door No.1, the host, who knows what lies behind each door, reveals
+          another door, say door No.3, which has a goat. The contestant is then
           offered two options. They can either pick another one of the two doors
-          that have not been revealed yet (door no.2), or stick with their
-          choice (door no.3).
-          
+          that have not been revealed yet (door No.2), or stick with their
+          choice (door No.3).
+        </p>
+        <div className="doors">
+          <Image
+            width={500}
+            height={500}
+            src={require("/public/threeDoors.png")}
+            alt="Image of three doors"
+          ></Image>
+        </div>
+        <p className="description">
+          Obviously, at first glance, the problem seems straightforward, you
+          might think that changing your choice after the reveal wouldn't really
+          make much of a difference. However, stastically, switching your
+          choice, increases your chances of winning to 2/3 as
+          opposed to the 1/3 when you don't switch doors.
         </p>
       </div>
     </>
