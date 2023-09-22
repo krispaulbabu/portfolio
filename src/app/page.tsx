2e621985da98c2a659f784d124705aa00d2a4b85
krispaulbabu <krispaulbabukkp@gmail.com>
2023-09-22 16:10:18 +1000

    Ok i'm switching things up a bit. I'm gonna be using this page as a portfolio instead. I've starting building the homepage. I need to get my shit together. The monty hall problem page can wait.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,10 @@ import Script from "next/script";
 export default function Home() {
   return (
     <>
+      <div className="hpBody">
+        <h1 className="hpHeader"> Hi!</h1>
+        <p className="hpDescription"> Welcome to my blog / portfolio </p>
+      </div>
     </>
   );
 }
