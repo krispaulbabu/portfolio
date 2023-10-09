c425fe7d984e0e05f38cc8f817b9099b97b984de
krispaulbabu <krispaulbabukkp@gmail.com>
2023-10-09 11:30:53 +1100

    I've made some massive changes. Started with the book finder thing again. I've made some headway. Managed to keep my google books api request within the allowed range. I've set the theme of the page too. Right now i'm working on the mechanism to save books to a collection.

--- src/pages/bookfinder.tsx
+++ src/pages/bookfinder.tsx
@@ -1,60 +1,181 @@
-import { TextField } from "@mui/material";
-import React, { useState } from "react";
-import { render } from "react-dom";
+import { IconButton, Tooltip } from "@mui/material";
+import React, { useEffect, useRef, useState } from "react";
+import "src/app/bookfinder.css";
+import SearchIcon from "@mui/icons-material/Search";
+import { Input } from "@mui/material";
+import AutoStoriesIcon from "@mui/icons-material/AutoStories";
+import CloseIcon from "@mui/icons-material/Close";
 
-export default function bookfinder() {
-  const [result, setResult] = useState([])
-  const [input, setInput]=useState("")
+let bookCollection = new Array();
 
-  const req = (searchTerm: String) => {
-    fetch("https://openlibrary.org/search.json?q="+searchTerm.replaceAll(" ","+"))
-      .then((response) => {
-        if (response.status === 200) {
-          return response.json();
-        } else {
-          // console.log(typeof(response))
-        }
-      })
-      .then((response) => {
-        setResult(response.docs);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  };
+export default function Bookfinder() {
+  const [result, setResult] = useState([]);
+  const [input, setInput] = useState("");
+  const [book, setSelected] = useState(null);
+  const [iconHover, setHover]=useState(null)
+
+  const iconRef=useRef(null)
+
+  useEffect(() => {
+    if (book != null) {
+      if (!bookCollection.includes(book)) {
+        bookCollection = [...bookCollection, book];
+        console.log(bookCollection);
+      } else {
+        bookCollection = bookCollection.filter((item) => item !== book);
+        console.log(bookCollection);
+      }
+      setSelected(null);
+    }
+  }, [book]);
+
+
+
+  async function reqs(searchTerm: string) {
+    try {
+      const response = await fetch(
+        "https://www.googleapis.com/books/v1/volumes?q=" +
+          searchTerm.replaceAll(" ", "+") +
+          "&maxResults=40"
+      );
+      const data = await response.json();
+      setResult(data.items);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   function renderResult() {
-    console.log(result.length)
-    if (result.length !== 0) {
+    if (typeof result !== "undefined" && result.length !== 0) {
+      // console.log(result);
       return (
-        <>
+        <div id="bookContainer">
           {result.map((values) => (
-              <label style={{color:"white"}}>{values["volumeInfo"]["title"]}</label>
+            <div key={values["id"]} className="book">
+              <Tooltip
+                title={
+                  <>
+                    <b>Title: </b>
+                    {values["volumeInfo"]["title"]}
+                    <br />
+                    <b>Author: </b>
+                    {typeof values["volumeInfo"]["authors"] != "undefined"
+                      ? values["volumeInfo"]["authors"][0]
+                      : "No author"}
+                    <br />
+                    <b>Description: </b>
+                    <p
+                      style={{
+                        textOverflow: "ellipsis",
+                        overflow: "hidden",
+                        whiteSpace: "nowrap",
+                      }}
+                    >
+                      {values["volumeInfo"]["description"]}
+                    </p>
+                  </>
+                }
+                followCursor
+                arrow
+              >
+                <img
+                  className="bookcover"
+                  onClick={() => {
+                    setSelected(values);
+                    // handleBookSelected();
+                    // console.log(bookCollection);
+                  }}
+                  src={
+                    typeof values["volumeInfo"]["imageLinks"] != "undefined"
+                      ? "https://books.google.com/books/publisher/content/images/frontcover/" +
+                        values["id"] +
+                        "?fife=w470-h600&source=gbs_api"
+                      : "https://islandpress.org/sites/default/files/default_book_cover_2015.jpg"
+                  }
+                ></img>
+              </Tooltip>
+              <label className="booktitle">
+                {" "}
+                {values["volumeInfo"]["title"]}{" "}
+              </label>{" "}
+              <br />
+              <label className="bookauthor">
+                {typeof values["volumeInfo"]["authors"] != "undefined"
+                  ? values["volumeInfo"]["authors"][0]
+                  : "No author"}
+              </label>
+            </div>
           ))}
-        </>
+        </div>
       );
     }
   }
 
+  function renderCollection() {
+    return (
+      <div id="collection">
+        {bookCollection.map((books) => (
+          <>
+            {/* <label style={{color:"black"}}>{books["id"]}</label><br/> */}
+            <img
+              src={
+                typeof books["volumeInfo"]["imageLinks"] != "undefined"
+                  ? "https://books.google.com/books/publisher/content/images/frontcover/" +
+                    books["id"] +
+                    "?fife=w20-h50&source=gbs_api"
+                  : "https://islandpress.org/sites/default/files/default_book_cover_2015.jpg"
+              }
+            ></img>
+          </>
+        ))}
+      </div>
+    );
+  }
+
+  useEffect(()=>{
+  },[])
   return (
     <>
       <div className="bookFinder">
-        <input
-          id="bookSearch"
+        <label id="quote">
+          “Beauty is about finding the right fit, the most natural fit. To be
+          perfect, you have to feel perfect about yourself—avoid trying to be
+          something you’re not.” - The Lost Hero
+        </label>
+        <Input
+          disableUnderline
+          startAdornment={<SearchIcon style={{ color: "#3e5c74" }} />}
+          endAdornment={<CloseIcon style={{ color: "#3e5c74" }} />}
+          className="bookSearch"
           onChange={(reply) => {
-            if(reply.target.value.length!=0) setInput(reply.target.value)
-            console.log(input)
+            if (reply.target.value.length !== 0) setInput(reply.target.value);
           }}
-          onKeyDown={(key)=>{
-            if(key.key=="Enter" && input.length!=0) {
-              console.log(input)
-              console.log("https://openlibrary.org/search.json?q="+input.replaceAll(" ","+"))
-              req(input)
-              console.log(result)
+          onKeyDown={async (key) => {
+            if (key.key === "Enter" && input.length !== 0) {
+              console.log(input);
+              console.log(
+                "https://openlibrary.org/search.json?q=" +
+                  input.replaceAll(" ", "+")
+              );
+              reqs(input);
             }
-            else{}
           }}
-        ></input>
+        />
+        <AutoStoriesIcon
+          id="collectionButton"
+          onMouseOver={()=>{
+            setHover(true)
+            console.log(iconHover)
+          }}
+          onMouseLeave={()=>{
+            setHover(false)
+            console.log(iconHover)
+          }}
+        />
+        {renderResult()}
+        {
+
+        }
       </div>
     </>
   );
