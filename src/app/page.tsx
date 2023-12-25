19d5bbc71b7ac7a42846ea09da34dcd7e6d41f22
krispaulbabu <krispaulbabukkp@gmail.com>
2023-12-25 19:04:21 +0400

    Changed all the apostrophes in page.tsx to apos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,26 +4,26 @@ export default function Home() {
   return (
     <>
       <div className="hpBody">
-        <h1 className="hpHeader"> Hi! I'm Kris!</h1>
+        <h1 className="hpHeader"> Hi! I&apos;m Kris!</h1>
         <p className="hpDescription">
-          Hello there! Thanks for stopping by my portfolio. I'm a 21-year-old
+          Hello there! Thanks for stopping by my portfolio. I&apos;m a 21-year-old
           software developer originally from the UAE, and I recently completed
           my Bachelor of Science degree with a specialization in Computing at
           Macquarie University. <br/><br/>
           
-          During my academic journey and beyond, I've
+          During my academic journey and beyond, I&apos;ve
           delved into the worlds of software development, data science, machine
-          learning, and statistical computing. I'm passionate about creating
+          learning, and statistical computing. I&apos;m passionate about creating
           efficient software solutions and using data to make informed
           decisions. <br/><br/>
           
-          In this portfolio, you'll find a collection of my projects,
-          each representing a unique aspect of my journey. I'm dedicated to
+          In this portfolio, you&apos;ll find a collection of my projects,
+          each representing a unique aspect of my journey. I&apos;m dedicated to
           pushing the boundaries of technology and exploring new possibilities.<br/><br/>
 
-          Whether you're here to view my work, discuss tech, or explore
-          potential collaborations, I'm here to connect. Please feel free to
-          browse my portfolio, and don't hesitate to reach out. Thank you for
+          Whether you&apos;re here to view my work, discuss tech, or explore
+          potential collaborations, I&apos;m here to connect. Please feel free to
+          browse my portfolio, and don&apos;t hesitate to reach out. Thank you for
           visiting, and I hope you find inspiration in my digital creations!<br/><br/>
         </p>
       </div>
