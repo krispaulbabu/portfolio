3b492739b4e0bd4398351c150d4f70272ef2900c
krispaulbabu <krispaulbabukkp@gmail.com>
2024-03-06 15:04:45 +1100

    aribnb link
    - added a link to the airbnb jupyter notebook page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,51 @@
 import Image from "next/image";
-
+import airbnb from "/public/airbnb.png";
 export default function Home() {
   return (
     <>
       <div className="hpBody">
         <h1 className="hpHeader"> Hi! I&apos;m Kris!</h1>
-        <p className="hpDescription">
-          Hello there! Thanks for stopping by my portfolio. I&apos;m a 21-year-old
-          software developer originally from the UAE, and I recently completed
-          my Bachelor of Science degree with a specialization in Computing at
-          Macquarie University. <br/><br/>
-          
-          During my academic journey and beyond, I&apos;ve
-          delved into the worlds of software development, data science, machine
-          learning, and statistical computing. I&apos;m passionate about creating
-          efficient software solutions and using data to make informed
-          decisions. <br/><br/>
-          
-          In this portfolio, you&apos;ll find a collection of my projects,
-          each representing a unique aspect of my journey. I&apos;m dedicated to
-          pushing the boundaries of technology and exploring new possibilities.<br/><br/>
-
-          Whether you&apos;re here to view my work, discuss tech, or explore
-          potential collaborations, I&apos;m here to connect. Please feel free to
-          browse my portfolio, and don&apos;t hesitate to reach out. Thank you for
-          visiting, and I hope you find inspiration in my digital creations!<br/><br/>
-        </p>
+        <div className="descDiv">
+          <p
+            style={{
+              borderRightWidth: "2px",
+              paddingRight: "10px",
+              width: "750px",
+            }}
+          >
+            Hello there! Thanks for stopping by my portfolio. I&apos;m a
+            21-year-old software developer originally from the UAE, and I
+            recently completed my Bachelor of Science degree with a
+            specialization in Computing at Macquarie University. <br />
+            <br />
+            During my academic journey and beyond, I&apos;ve delved into the
+            worlds of software development, data science, machine learning, and
+            statistical computing. I&apos;m passionate about creating efficient
+            software solutions and using data to make informed decisions. <br />
+            <br />
+            In this portfolio, you&apos;ll find a collection of my projects,
+            each representing a unique aspect of my journey. I&apos;m dedicated
+            to pushing the boundaries of technology and exploring new
+            possibilities.
+            <br />
+            <br />
+            Whether you&apos;re here to view my work, discuss tech, or explore
+            potential collaborations, I&apos;m here to connect. Please feel free
+            to browse my portfolio, and don&apos;t hesitate to reach out. Thank
+            you for visiting, and I hope you find inspiration in my digital
+            creations!
+            <br />
+            <br />
+          </p>
+          <div className="projectList">
+          <a href="/main.html">
+            <div className="project">
+              <img src={"/airbnb.png"} style={{ width: 60, height:60, marginRight:"10px", marginTop:"10px", borderRadius:"15px"}} />
+              <h5>Airbnb Data exploration</h5>
+            </div>
+            </a>
+          </div>
+        </div>
       </div>
     </>
   );
---
+++ src/pages/airbnb_analysis.tsx
@@ -0,0 +1,6 @@
+
+export default function airbnb() {
+  return(<>
+          <div className="content" dangerouslySetInnerHTML={{__html: "/public/main.html"}}></div>
+  </>)
+}
