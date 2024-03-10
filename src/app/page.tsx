c830704675c940cc35bfc4d9fd0d21830d0201f3
krispaulbabu <krispaulbabukkp@gmail.com>
2024-03-11 03:32:53 +1100

    major changes to project section
    - added a gallery of sorts for my future projects, with only the airbnb for now
    
    - when hovering over projects, a display slides in (empty right now)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
           <a href="#about">ABOUT</a>
         </li>
         <li>
-          <a href="contact.asp">PROJECTS</a>
+          <a href="#projects">PROJECTS</a>
         </li>
         <li>
           <a href="about.asp">CONTACT</a>
@@ -17,49 +17,58 @@ export default function Home() {
       </ul>
       <div className="hpBody" id="home">
         <h1 className="hpHeader"> Hi! I&apos;m Kris!</h1>
-        {/* <div className="descDiv"> */}
-          {/* <div className="projectList">
+      </div>
+      <div className="descDiv" id="about">
+        <p style={{}}>
+          Hello there! Thanks for stopping by my portfolio. I&apos;m a
+          21-year-old software developer originally from the UAE, and I recently
+          completed my Bachelor of Science degree with a specialization in
+          Computing at Macquarie University. <br />
+          <br />
+          During my academic journey and beyond, I&apos;ve delved into the
+          worlds of software development, data science, machine learning, and
+          statistical computing. I&apos;m passionate about creating efficient
+          software solutions and using data to make informed decisions. <br />
+          <br />
+          In this portfolio, you&apos;ll find a collection of my projects, each
+          representing a unique aspect of my journey. I&apos;m dedicated to
+          pushing the boundaries of technology and exploring new possibilities.
+          <br />
+          <br />
+          Whether you&apos;re here to view my work, discuss tech, or explore
+          potential collaborations, I&apos;m here to connect. Please feel free
+          to browse my portfolio, and don&apos;t hesitate to reach out. Thank
+          you for visiting, and I hope you find inspiration in my digital
+          creations!
+          <br />
+          <br />
+        </p>
+      </div>
+      <div className="projects" id="projects">
+        <div className="gallery">
           <a href="/main.html">
             <div className="project">
-              <img src={"/airbnb.png"} style={{ width: 60, height:60, marginRight:"10px", marginTop:"10px", borderRadius:"15px"}} />
-              <div style={{display:"flex", flexDirection:"column"}}>
-              <h5 style={{marginTop:"8px", fontWeight:500}}>Airbnb Data exploration</h5>
-              <p style={{fontSize:12, marginRight:"3px"}}>An exploratory analysis of Airbnb data from kaggle using python, jupyter notebook and tableau.</p>
-              </div>
+              <img
+                src="airbnb.png"
+                style={{
+                  height: "200px",
+                  width: "200px",
+                  borderRadius: "20px",
+                }}
+              />
+
+              <div className="overlay"></div>
             </div>
-            </a>
-          </div> */}
-        {/* </div> */}
-      </div>
-      <div className="descDiv"
-      id="about"
-      >
-          <p style={{}}>
-            Hello there! Thanks for stopping by my portfolio. I&apos;m a
-            21-year-old software developer originally from the UAE, and I
-            recently completed my Bachelor of Science degree with a
-            specialization in Computing at Macquarie University. <br />
-            <br />
-            During my academic journey and beyond, I&apos;ve delved into the
-            worlds of software development, data science, machine learning, and
-            statistical computing. I&apos;m passionate about creating efficient
-            software solutions and using data to make informed decisions. <br />
-            <br />
-            In this portfolio, you&apos;ll find a collection of my projects,
-            each representing a unique aspect of my journey. I&apos;m dedicated
-            to pushing the boundaries of technology and exploring new
-            possibilities.
-            <br />
-            <br />
-            Whether you&apos;re here to view my work, discuss tech, or explore
-            potential collaborations, I&apos;m here to connect. Please feel free
-            to browse my portfolio, and don&apos;t hesitate to reach out. Thank
-            you for visiting, and I hope you find inspiration in my digital
-            creations!
-            <br />
-            <br />
-          </p>
+          </a>
+          <div className="project"></div>
+          <div className="project"></div>
+          <div className="project"></div>
+          <div className="project"></div>
+          <div className="project"></div>
+          <div className="project"></div>
+          <div className="project"></div>
         </div>
+      </div>
     </>
   );
 }
