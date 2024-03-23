f8c34a4feccd1bef408220726cd090c8bdc9e504
krispaulbabu <krispaulbabukkp@gmail.com>
2024-03-23 18:26:19 +1100

    removed useeffect from testflask because it caused an error

--- src/pages/testflask.tsx
+++ src/pages/testflask.tsx
@@ -1,12 +1,4 @@
-import { useEffect } from "react";
 export default function testflask() {
-
-
-  useEffect(() => {  
-    fetch("/something").
-    then((result)=>{result.json()})
-    .then((data)=>{console.log(data)})
-  });
 
   return (<>
     something
