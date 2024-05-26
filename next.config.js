b323972fd7a032feef2a15de688c6565ecd4c20c
krispaulbabu <krispaulbabukkp@gmail.com>
2024-05-26 22:49:30 +1000

    another test

--- next.config.js
+++ next.config.js
@@ -8,14 +8,17 @@ const nextConfig = {
       },
     ],
   },
-  rewrites() {
-    return [
-      {
-        source: '/bookfinder',
-        destination: 'https://krispaulbabu.com/bookfinder',
-      },
-    ]
-  }
+  async rewrites() {
+    // Check if the environment variable for the subdomain is set
+    if (process.env.SUBDOMAIN === 'BOOKFINDER') {
+      return [
+        { source: '/', destination: '/bookfinder' },
+      ];
+    }
+
+    // Default rewrites for the main domain
+    return [];
+  },
 }
 
 module.exports = nextConfig
