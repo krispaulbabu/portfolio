6dcf3084e66bf943979c894833065c93595f7620
krispaulbabu <krispaulbabukkp@gmail.com>
2024-05-31 12:49:22 +1000

    added modal screen for book info

--- src/functions/bookfinder/renderResult.tsx
+++ src/functions/bookfinder/renderResult.tsx
@@ -2,9 +2,18 @@ import { Tooltip } from "@mui/material";
 import Image from "next/image";
 import toTitleCase from "./titleCase";
 import Fab from "@mui/material/Fab";
-import { useState } from "react";
-
 import { Add, Remove, LocalLibrary } from "@mui/icons-material";
+import { useDisclosure } from "@nextui-org/react";
+import { useEffect, useState } from "react";
+import Rating from "@mui/material/Rating";
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+} from "@nextui-org/modal";
+import { Button } from "@nextui-org/react";
 
 export default function renderResult(
   result: any,
@@ -15,7 +24,16 @@ export default function renderResult(
   buttonStates: any,
   toggleButtonState: any
 ) {
+  const [modalStates, setModalState] = useState<{ [id: string]: boolean }>({});
 
+  const toggleModal = (id: string) => {
+    setModalState((prev: any) => ({
+      ...prev,
+      [id]: !prev[id],
+    }));
+  };
+
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
   if (typeof result !== "undefined" && result.length !== 0) {
     return (
       <div id="bookContainer">
@@ -79,13 +97,93 @@ export default function renderResult(
                   >
                     {buttonStates[values["id"]] ? <Remove /> : <Add />}
                   </Fab>
-
-                  <Fab className="importcontacts">
+                  <Fab
+                    className="importcontacts"
+                    onClick={() => {
+                      toggleModal(values.id);
+                      console.log(values);
+                    }}
+                  >
                     <LocalLibrary />
                   </Fab>
                 </div>
               </div>
             </Tooltip>
+            <Modal
+              motionProps={{
+                variants: {
+                  enter: {
+                    scale: 1,
+                    opacity: 1,
+                    transition: {
+                    },
+                  },
+                  exit: {
+                    scale: 0.0,
+                    opacity: 0,
+                    transition: {
+                    },
+                  },
+                },
+              }}
+              backdrop="blur"
+              className="modal"
+              data-backdrop="static"
+              isOpen={modalStates[values.id] || false}
+              onClose={() => toggleModal(values.id)}
+              hideCloseButton={true}
+            >
+              <ModalContent className="modalcontent">
+                <div className="modalImageAndTitle">
+                  <div>
+                    <Image
+                      alt="book cover image"
+                      height={300}
+                      width={235}
+                      className="modalImage"
+                      src={
+                        values["volumeInfo"]["imageLinks"]
+                          ? "https://books.google.com/books/publisher/content/images/frontcover/" +
+                            values["id"] +
+                            "?fife=w470-h600&source=gbs_api"
+                          : "https://islandpress.org/sites/default/files/default_book_cover_2015.jpg"
+                      }
+                    />
+                    <Button
+                      disableRipple
+                      className="closeButton"
+                      onClick={() => toggleModal(values.id)}
+                    >
+                      Close
+                    </Button>
+                  </div>
+                  <div>
+                    {" "}
+                    <ModalHeader className="modaltitle">
+                      {toTitleCase(values["volumeInfo"]["title"])}
+                    </ModalHeader>
+                    <h3 className="modalauthor">
+                      {values["volumeInfo"]["authors"]
+                        ? values["volumeInfo"]["authors"][0]
+                        : "No author"}
+                    </h3>
+                    <ModalBody className="modalDesc">
+                      {values["volumeInfo"]["description"]}
+                    </ModalBody>
+                    {/* <div className="modalMaturityRating">
+                      {values["volumeInfo"]["maturityRating"]}
+                    </div> */}
+                    <label className="categories" style={{marginLeft:"3vw"}}>
+                      Genre: {values["volumeInfo"]["categories"]
+                        ? values["volumeInfo"]["categories"]
+                        : "No categories"} | 
+                    </label>
+                    <label className="pagecount"> Page count: {values["volumeInfo"]["pageCount"]}</label>
+                  </div>
+                </div>
+                <ModalFooter></ModalFooter>
+              </ModalContent>
+            </Modal>
             <label className="booktitle">
               {toTitleCase(values["volumeInfo"]["title"])}
             </label>
